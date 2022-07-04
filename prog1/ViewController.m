25371118ff2d1bd32553dcedb22aa267c2440181
DiepKinh <thudiep1122pk@gmail.com>
2022-07-04 10:39:13 +0700

    Initial Commit

---
+++ prog1/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  prog1
+//
+//  Created by DiepKinh on 04/07/2022.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
